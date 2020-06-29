75b19d947d7b57ccad9c6585e9b0585513632d24
Aaron Sinnott <aaronsnig@gmail.com>
2020-06-29 19:53:39 -0300

    Refactor the ecommand to use text to text endpoint (#11)

--- src/commands/translate_text.ts
+++ src/commands/translate_text.ts
@@ -2,7 +2,6 @@ import {Command, flags} from '@oclif/command'
 import axios from 'axios'
 
 interface ApiData {
-  initial_language_code: string,
   target_language_code: string,
   text: string
 }
@@ -16,28 +15,28 @@ export default class TranslateText extends Command {
 
   static flags = {
     help: flags.help({char: 'h'}),
-    // flag to determine the language of the text provided
-    existing_lang: flags.string({char: 'l', description: 'Four-character code for the target language'}),
     // flag to determine the target language
     target_lang: flags.string({char: 't', description: 'Two-character code for the target language'})
   }
 
   static args = [{name: 'text'}]
 
-  async callApi(initial_language_code:any, target_language_code:any, text:any) {
+  async callApi(target_language_code:any, text:any) {
     let data: ApiData = {
-      "initial_language_code": initial_language_code,
       "target_language_code": target_language_code,
       "text": text
     }
-    return axios.post('https://decyphr.uc.r.appspot.com/api/v1/plain-text/', data)
-      .then(response => console.log(response))
+    return axios.post('https://decyphr.uc.r.appspot.com/api/v1/text-to-text/', data)
+      .then(response => {
+        console.log("Response retrieved successfully")
+        return response.data
+      })
       .catch(error => console.log(error))
   }
 
   async run() {
     const {args, flags} = this.parse(TranslateText)
-    let response = this.callApi(flags.existing_lang, flags.target_lang, args.text)
-    this.log(`You are trying to translate "${args.text}" from "${flags.existing_lang}" to "${flags.target_lang}"`)
+    let response = await this.callApi(flags.target_lang, args.text)
+    this.log(`"${args.text}" translates to "${response.translated_text}"`)
   }
 }
