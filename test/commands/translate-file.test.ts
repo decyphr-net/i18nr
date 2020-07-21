3126aaf3936ffa35ebae0c714d2eb7548de2afca
Aaron Sinnott <aaronsnig@gmail.com>
2020-07-21 14:44:21 -0300

    Add translation files for testing and development

--- test/commands/translate-file.test.ts
+++ test/commands/translate-file.test.ts
@@ -3,8 +3,8 @@ import {expect, test} from '@oclif/test'
 describe('translate-file', () => {
   test
   .stdout()
-  .command(['translate-file', 'en.json', '--target_lang', 'pt'])
-  .it('runs translate-file en.json --target_lang pt', ctx => {
+  .command(['translate-file', 'i18n/en.json', '--target_lang', 'pt'])
+  .it('runs translate-file i18n/en.json --target_lang pt', ctx => {
     expect(ctx.stdout).to.contain('task complete')
   })
 })
